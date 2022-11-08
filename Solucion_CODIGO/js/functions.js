447c2a7b56ffce683d450981bdfb4b9e121d9529
8444-arfonseca1 <58662403+8444-arfonseca1@users.noreply.github.com>
2022-11-08 09:07:55 -0500

    PRIMERA PARTE

--- Solucion_CODIGO/js/functions.js
+++
@@ -1,6 +0,0 @@
-function hidefrm(){
-    document.getElementById('FormConfidencial').style.display ='none';
-  }
-  function showfrm(){
-    document.getElementById('FormConfidencial').style.display = 'block';
-  }
--- Solucion_CODIGO/js/main.js
+++ Solucion_CODIGO/js/main.js
@@ -45,3 +45,9 @@ $(document).ready(function(){
 
 });
 
+function hidefrm(){
+    document.getElementById('FormConfidencial').style.display ='none';
+  }
+  function showfrm(){
+    document.getElementById('FormConfidencial').style.display = 'block';
+  }
