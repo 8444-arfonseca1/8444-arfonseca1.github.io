2389bfb5cda863c81f0f81812e0fa48bb18a42fb
8444-arfonseca1 <58662403+8444-arfonseca1@users.noreply.github.com>
2022-11-08 10:30:29 -0500

    PRIMERA PARTE

---
+++ Solucion_CODIGO/js/ocultar_frm.js
@@ -0,0 +1,8 @@
+// Mostrar y ocultar formulario
+function hidefrm() {
+    document.getElementById("FormConfidencial").style.display = "none";
+  }
+
+  function showfrm() {
+    document.getElementById("FormConfidencial").style.display = "block";
+  }
