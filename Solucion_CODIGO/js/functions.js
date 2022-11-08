2344b59699b66fa452a6a4f1537a35864c9c4727
8444-arfonseca1 <58662403+8444-arfonseca1@users.noreply.github.com>
2022-11-08 09:05:58 -0500

    SEGUNDA PARTE

---
+++ Solucion_CODIGO/js/functions.js
@@ -0,0 +1,6 @@
+function hidefrm(){
+    document.getElementById('FormConfidencial').style.display ='none';
+  }
+  function showfrm(){
+    document.getElementById('FormConfidencial').style.display = 'block';
+  }
